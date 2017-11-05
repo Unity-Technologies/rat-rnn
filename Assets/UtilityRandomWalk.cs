5db541fec49b9c0c4f617ebdee99ffcdc31fc367
Renaldas Zioma <rej@unity3d.com>
2017-11-05 22:56:56 +0100

    Turn away from walls using whisker sensors

--- Assets/UtilityRandomWalk.cs
+++ Assets/UtilityRandomWalk.cs
@@ -1,14 +1,35 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Linq;
 
 public class UtilityRandomWalk : UtilityBase {
 
 	public float runSpeed = .1f;
-	public float turnSpeed = 30f;
+	public float turnSpeed = 5f;
+
+	public bool turnAwayFromWalls = true;
 
 	protected override Vector2 DoControl(float[] sensors)
 	{
-		var inputX = Random.Range(-1f, 1f);
+		var leftX = 1f;
+		var rightX = 1f;
+
+		if (turnAwayFromWalls)
+		{
+			var n = sensors.Length / 2;
+			float l = sensors.Take(n).Reverse().Count(s => s < 0.1f);
+			float r = sensors.Skip(n).Count(s => s < 0.1f);
+
+			leftX = Mathf.Lerp(-1f, 1f, l / n);
+			rightX = Mathf.Lerp(-1f, 1f, r / n);
+
+			if (-leftX > rightX)
+			{
+				leftX = -1f;
+				rightX = 1f;
+			}
+		}
+
+		var inputX = Random.Range(-leftX, rightX);
 		var inputY = Random.Range(0, 1f);
 
 		return new Vector2(
