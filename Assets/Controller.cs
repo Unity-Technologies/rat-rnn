95d7fa5604bf4f0befb48af4a39e44559064a612
Renaldas Zioma <rej@unity3d.com>
2017-11-05 22:56:34 +0100

    Whisker sensors

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -7,18 +7,22 @@ public class Controller : MonoBehaviour {
 	public float maxRunSpeed = .1f;
 	public float maxTurnSpeed = 10f;
 
+	public int whiskersCount = 10;
+	public float whiskersAngle = 120;
+	public float whiskersLength = 1;
+
 	private Rigidbody rb;
+	private float bodyRadius;
 
 	void Start()
 	{
 		rb = GetComponent<Rigidbody>();
+		bodyRadius = GetComponent<Collider>().bounds.extents.z;
 	}
 
 	void FixedUpdate()
 	{
-		var sensors = new float[0];
-
-		// @TODO: whiskers
+		var sensors = ReadSensors();
 
 		var utilities = GetComponents<UtilityBase>();
 		var totalStrength = utilities.Sum(u => u.strength);
@@ -30,5 +34,25 @@ public class Controller : MonoBehaviour {
 
 		rb.angularVelocity = Vector3.up * Mathf.Deg2Rad * Mathf.Clamp(control.x, -maxTurnSpeed, maxTurnSpeed) / Time.deltaTime;
 		rb.velocity = transform.forward * Mathf.Clamp(control.y, 0, maxRunSpeed) / Time.deltaTime;
+	}
+
+	float[] ReadSensors()
+	{
+		var sensors = new float[whiskersCount];
+
+		var angle = -whiskersAngle / 2.0f;
+		for (int q = 0; q < whiskersCount; ++q, angle += whiskersAngle / whiskersCount)
+		{
+			var dir = Quaternion.Euler(0, angle, 0) * transform.forward;
+			var orig = transform.position + dir * bodyRadius;
+
+			RaycastHit hit;
+			if (Physics.Raycast(orig, dir, out hit, whiskersLength))
+				sensors[q] = 1.0f - hit.distance / whiskersLength;
+
+			Debug.DrawLine(orig, orig + dir * whiskersLength, sensors[q] > 0 ? Color.red: Color.green, 0, false);
+		}
+
+		return sensors;
 	}
 }
