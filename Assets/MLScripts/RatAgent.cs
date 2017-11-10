fc78a7b6d1ae1bd159b158329e8cf510d58f3464
Arthur Juliani <awjuliani@gmail.com>
2017-11-10 11:12:01 -0800

    Clean up formatting

--- Assets/MLScripts/RatAgent.cs
+++ Assets/MLScripts/RatAgent.cs
@@ -2,36 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RatAgent : Agent {
+public class RatAgent : Agent
+{
 
 
 
-	public override List<float> CollectState()
-	{
-		List<float> state = new List<float>();
+    public override List<float> CollectState()
+    {
+        List<float> state = new List<float>();
         state.Add(transform.position.x);
         state.Add(transform.position.z);
         state.Add(transform.rotation.y);
         state.Add(GetComponent<Rigidbody>().velocity.x);
         state.Add(GetComponent<Rigidbody>().velocity.z);
-		return state;
-	}
+        return state;
+    }
 
-	public override void AgentStep(float[] act)
-	{
+    public override void AgentStep(float[] act)
+    {
 
-	}
+    }
 
-	public override void AgentReset()
-	{
+    public override void AgentReset()
+    {
         transform.position = new Vector3(0f, 0f, 0f);
         transform.rotation = Quaternion.Euler(new Vector3(0f, -90f, 0f));
         GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
         GetComponent<Rigidbody>().angularVelocity = new Vector3(0f, 0f, 0f);
-;	}
+    }
 
-	public override void AgentOnDone()
-	{
+    public override void AgentOnDone()
+    {
 
-	}
+    }
 }
