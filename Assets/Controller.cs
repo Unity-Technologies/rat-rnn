e72b5e929101381f6aef0b47a8af4fe3242ef874
Renaldas Zioma <rej@unity3d.com>
2017-11-12 11:40:21 +0200

    * Don’t depend on Time.time inside DoControl since it is not incremented during FixedTime() calls * runSpeed and turnSpeed only in Controller. Utilities report normalised controls. * Take into account isActiveAndEnabled * Save experiment data to JSON file * Renamed strength => priority * More descriptive var names: rb => body

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -1,22 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System;
 using System.Linq;
+using System.IO;
 
 public class Controller : MonoBehaviour {
 	
-	public float maxRunSpeed = .1f;
-	public float maxTurnSpeed = 10f;
+	public float runSpeed = .1f;
+	public float turnSpeed = 10f;
 
 	public int whiskersCount = 10;
 	public float whiskersAngle = 120;
 	public float whiskersLength = 1;
 
-	private Rigidbody rb;
+	private Rigidbody body;
 	private float bodyRadius;
 
 	void Start()
 	{
-		rb = GetComponent<Rigidbody>();
+		body = GetComponent<Rigidbody>();
 		bodyRadius = GetComponent<Collider>().bounds.extents.z;
 	}
 
@@ -25,15 +27,23 @@ public class Controller : MonoBehaviour {
 		var sensors = ReadSensors();
 
 		var utilities = GetComponents<UtilityBase>();
-		var totalStrength = utilities.Sum(u => u.strength);
-		totalStrength = Mathf.Max(totalStrength, Mathf.Epsilon);
+		var sumPriority = utilities.Sum(u => u.priority * (u.isActiveAndEnabled ? 1: 0f));
+		sumPriority = Mathf.Max(sumPriority, Mathf.Epsilon);
 
 		Vector2 control = Vector2.zero;
 		foreach (var u in utilities)
-			control += u.Control (sensors) * u.strength / totalStrength;
+			if (u.isActiveAndEnabled)
+				control += u.Control (sensors) * u.priority / sumPriority;
 
-		rb.angularVelocity = Vector3.up * Mathf.Deg2Rad * Mathf.Clamp(control.x, -maxTurnSpeed, maxTurnSpeed) / Time.deltaTime;
-		rb.velocity = transform.forward * Mathf.Clamp(control.y, 0, maxRunSpeed) / Time.deltaTime;
+		body.angularVelocity = Vector3.up * Mathf.Deg2Rad * Mathf.Clamp(control.x, -1f, 1f) * turnSpeed;
+		body.velocity = transform.forward * Mathf.Clamp01(control.y) * runSpeed;
+
+		RecordState(
+			Mathf.Clamp(control.x, -1f, 1f), Mathf.Clamp01(control.y),	// in
+			transform.position.x, transform.position.z,					// out
+			sensors														// aux
+		);
+
 	}
 
 	float[] ReadSensors()
@@ -54,5 +64,78 @@ public class Controller : MonoBehaviour {
 		}
 
 		return sensors;
+	}
+
+	System.Collections.Generic.List<StateEntry> entries = new System.Collections.Generic.List<StateEntry>(1000);
+	void RecordState(float heading, float velocity, float x, float y, float[] sensors)
+	{	
+		entries.Add(
+			new StateEntry{
+				heading = heading, velocity = velocity,
+				x = x, y = y, 
+				sensors = sensors
+			});
+
+		if (entries.Count > 100)
+			SaveExperiment();
+	}
+
+	void SaveExperiment()
+	{
+		var experiment = new Experiment{
+			runSpeed = runSpeed,
+			turnSpeed = turnSpeed,
+			bodyRadius = bodyRadius,
+			sceneName = SceneManager.GetActiveScene().name,
+			environmentName = GameObject.FindGameObjectWithTag("Maze").name,
+			dateTime = DateTime.UtcNow.ToString(),
+			entries = entries.ToArray()
+		};
+		var json = JsonUtility.ToJson(experiment);
+
+		var filename = "experiment.json";
+		using (var fs = new FileStream(filename, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+		{
+			bool newFile = fs.Length == 0;
+
+			if (!newFile)
+				fs.Seek(-1, SeekOrigin.End); // remove closing bracket, wwe are going to add entry to the array
+
+			using (var writer = new StreamWriter(fs))
+			{
+				if (newFile)
+					writer.Write("[\n");
+				else
+					writer.Write(",\n");
+				writer.Write(json);
+				writer.Write("]");
+			}
+		}
+
+		entries.Clear();
+	}
+
+	void OnApplicationQuit()
+	{
+		SaveExperiment();
+	}
+
+	class Experiment
+	{
+		public float runSpeed;
+		public float turnSpeed;
+		public float bodyRadius;
+		public string sceneName;
+		public string environmentName;
+		public string dateTime;
+		public StateEntry[] entries;
+	}
+
+	[Serializable]
+	struct StateEntry
+	{
+		public float heading, velocity;
+		public float x, y;
+		public float[] sensors;
 	}
 }
--- Assets/UtilityBase.cs
+++ Assets/UtilityBase.cs
@@ -3,24 +3,24 @@ using System.Collections;
 
 public abstract class UtilityBase : MonoBehaviour {
 
-	public float strength = 1f;
+	public float priority = 1f;
 	public float decisionFrequency = 10f;
-	private float nextUpdate = -1f;
+	private double nextUpdate = 0f;
 	private Vector2 memory = Vector2.zero;
 
 	public Vector2 Control(float[] sensors)
 	{
 		Vector2 motors = Vector2.zero;
 
-		if (nextUpdate < 0)
-			nextUpdate = Time.time;
-
 		int iterations = 0;
-		while (nextUpdate <= Time.time && ++iterations <= 10)
+		nextUpdate -= (double)Time.fixedDeltaTime;
+		while (nextUpdate < 0.0 && ++iterations <= 10)
 		{
-			nextUpdate += 1.0f / decisionFrequency;
+			nextUpdate += 1.0 / (double)decisionFrequency;
 			motors += DoControl(sensors);
 		}
+		if (nextUpdate < 0)
+			nextUpdate = 0;
 
 		// if decisions were made => store in memory
 		if (iterations > 0)
