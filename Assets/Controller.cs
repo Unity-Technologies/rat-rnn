9f746e94177fd1954ec0f97644ba97d8c366301e
Renaldas Zioma <rej@unity3d.com>
2017-11-13 19:49:53 +0100

    Extracted custom .JSON saving code into separate RecordExperiment class

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -1,8 +1,6 @@
 ﻿using UnityEngine;
-using UnityEngine.SceneManagement;
 using System;
 using System.Linq;
-using System.IO;
 
 public class Controller : MonoBehaviour {
 	
@@ -15,6 +13,17 @@ public class Controller : MonoBehaviour {
 
 	private Rigidbody body;
 	private float bodyRadius;
+
+	private ObservableState state;
+	public ObservableState observableState { get { return state; } }
+
+	[Serializable]
+	public struct ObservableState
+	{
+		public float heading, velocity;
+		public float x, y;
+		public float[] sensors;
+	}
 
 	void Start()
 	{
@@ -38,12 +47,11 @@ public class Controller : MonoBehaviour {
 		body.angularVelocity = Vector3.up * Mathf.Deg2Rad * Mathf.Clamp(control.x, -1f, 1f) * turnSpeed;
 		body.velocity = transform.forward * Mathf.Clamp01(control.y) * runSpeed;
 
-		RecordState(
-			Mathf.Clamp(control.x, -1f, 1f), Mathf.Clamp01(control.y),	// in
-			transform.position.x, transform.position.z,					// out
-			sensors														// aux
-		);
-
+		state = new ObservableState {
+			heading = Mathf.Clamp(control.x, -1f, 1f), velocity = Mathf.Clamp01(control.y),		// in
+			x = transform.position.x, y = transform.position.y, 								// out
+			sensors = sensors																	// aux
+		};
 	}
 
 	float[] ReadSensors()
@@ -64,78 +72,5 @@ public class Controller : MonoBehaviour {
 		}
 
 		return sensors;
-	}
-
-	System.Collections.Generic.List<StateEntry> entries = new System.Collections.Generic.List<StateEntry>(1000);
-	void RecordState(float heading, float velocity, float x, float y, float[] sensors)
-	{	
-		entries.Add(
-			new StateEntry{
-				heading = heading, velocity = velocity,
-				x = x, y = y, 
-				sensors = sensors
-			});
-
-		if (entries.Count > 100)
-			SaveExperiment();
-	}
-
-	void SaveExperiment()
-	{
-		var experiment = new Experiment{
-			runSpeed = runSpeed,
-			turnSpeed = turnSpeed,
-			bodyRadius = bodyRadius,
-			sceneName = SceneManager.GetActiveScene().name,
-			environmentName = GameObject.FindGameObjectWithTag("Maze").name,
-			dateTime = DateTime.UtcNow.ToString(),
-			entries = entries.ToArray()
-		};
-		var json = JsonUtility.ToJson(experiment);
-
-		var filename = "experiment.json";
-		using (var fs = new FileStream(filename, FileMode.OpenOrCreate, FileAccess.ReadWrite))
-		{
-			bool newFile = fs.Length == 0;
-
-			if (!newFile)
-				fs.Seek(-1, SeekOrigin.End); // remove closing bracket, wwe are going to add entry to the array
-
-			using (var writer = new StreamWriter(fs))
-			{
-				if (newFile)
-					writer.Write("[\n");
-				else
-					writer.Write(",\n");
-				writer.Write(json);
-				writer.Write("]");
-			}
-		}
-
-		entries.Clear();
-	}
-
-	void OnApplicationQuit()
-	{
-		SaveExperiment();
-	}
-
-	class Experiment
-	{
-		public float runSpeed;
-		public float turnSpeed;
-		public float bodyRadius;
-		public string sceneName;
-		public string environmentName;
-		public string dateTime;
-		public StateEntry[] entries;
-	}
-
-	[Serializable]
-	struct StateEntry
-	{
-		public float heading, velocity;
-		public float x, y;
-		public float[] sensors;
 	}
 }
---
+++ Assets/RecordExperiment.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public class RecordExperiment : MonoBehaviour
+{
+	public string filename = "experiment";
+	public int length = 50000;
+	public int flushToFileEvery = 10000;
+	public Controller agent;
+
+	public int screenResolution = 64;
+
+	private int fixedUpdateCounter = 0;
+	private List<Controller.ObservableState> entries = new List<Controller.ObservableState>();
+
+	void Start()
+	{
+		if (agent == null)
+			agent = GameObject.FindObjectOfType<Controller>();
+
+		entries = new List<Controller.ObservableState>(flushToFileEvery);
+
+		Screen.SetResolution(screenResolution, screenResolution, false);
+	}
+
+	void FixedUpdate()
+	{
+		if (fixedUpdateCounter++ > length)
+			Application.Quit();
+
+		if (agent == null)
+			return;
+		
+		entries.Add(agent.observableState);
+
+		if (entries.Count > flushToFileEvery)
+		{
+			Save(agent, entries.ToArray());
+			entries.Clear();
+		}
+	}
+
+	void Save(Controller src, Controller.ObservableState[] entries)
+	{
+		var experiment = new Experiment{
+			runSpeed = src.runSpeed,
+			turnSpeed = src.turnSpeed,
+			bodyRadius = src.gameObject.GetComponent<Collider>().bounds.extents.z,
+			sceneName = SceneManager.GetActiveScene().name,
+			environmentName = GameObject.FindGameObjectWithTag("Maze").name,
+			dateTime = DateTime.UtcNow.ToString(),
+			entries = entries
+		};
+		var json = JsonUtility.ToJson(experiment);
+
+		using (var fs = new FileStream(filename + ".json", FileMode.OpenOrCreate, FileAccess.ReadWrite))
+		{
+			bool newFile = fs.Length == 0;
+
+			if (!newFile)
+				fs.Seek(-1, SeekOrigin.End); // remove closing bracket, wwe are going to add entry to the array
+
+			using (var writer = new StreamWriter(fs))
+			{
+				if (newFile)
+					writer.Write("[\n");
+				else
+					writer.Write(",\n");
+				writer.Write(json);
+				writer.Write("]");
+			}
+		}
+	}
+
+	void OnApplicationQuit()
+	{
+		if (agent == null)
+			return;
+		
+		Save(agent, entries.ToArray());
+	}
+
+	class Experiment
+	{
+		public float runSpeed;
+		public float turnSpeed;
+		public float bodyRadius;
+		public string sceneName;
+		public string environmentName;
+		public string dateTime;
+		public Controller.ObservableState[] entries;
+	}
+}
